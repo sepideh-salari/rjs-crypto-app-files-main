9d03d3fd36eec55e85460b140df8033aca721f17
sepideh-salari <salari.sepideh@gmail.com>
2024-12-02 22:04:35 +0330

    modal added

--- src/Componenets/Modules/Chart.jsx
+++ src/Componenets/Modules/Chart.jsx
@@ -12,15 +12,49 @@ import {
 } from "recharts";
 function Chart({ chart, setChart }) {
   const [type, setType] = useState("prices");
-  console.log(convertData(chart, type));
+  const typeHandler = (event) => {
+    if (event.target.tagName === "BUTTON") {
+      const type = event.target.innerText.toLowerCase().replace(" ", "_");
+      setType(type);
+    }
+  };
   return (
     <div className={styles.container}>
       <span className={styles.cross} onClick={() => setChart(null)}>
         X
       </span>
       <div className={styles.chart}>
+        <div className={styles.name}>
+          <img src={chart.coin.image} />
+          <p>{chart.coin.name}</p>
+        </div>
         <div className={styles.graph}>
           <ChartComponenet data={convertData(chart, type)} type={type} />
+        </div>
+        <div className={styles.types} onClick={typeHandler}>
+          <button className={type === "prices" ? styles.selected : null}>
+            Prices
+          </button>
+          <button className={type === "market_caps" ? styles.selected : null}>
+            Market Caps
+          </button>
+          <button className={type === "total_volumes" ? styles.selected : null}>
+            Total Volumes
+          </button>
+        </div>
+        <div className={styles.details}>
+          <div>
+            <p>Prices:</p>
+            <span>${chart.coin.current_price}</span>
+          </div>
+          <div>
+            <p>ATH:</p>
+            <span>${chart.coin.ath}</span>
+          </div>
+          <div>
+            <p>Market Cap:</p>
+            <span>{chart.coin.market_cap}</span>
+          </div>
         </div>
       </div>
     </div>
@@ -31,7 +65,17 @@ export default Chart;
 const ChartComponenet = ({ data, type }) => {
   return (
     <ResponsiveContainer width="100%" height="100%">
-      <LineChart width={400} height={400} data={data}>
+      <LineChart
+        width={400}
+        height={400}
+        data={data}
+        margin={{
+          top: 5,
+          right: 30,
+          left: 90,
+          bottom: 5,
+        }}
+      >
         <Line
           type="monotone"
           dataKey={type}
@@ -40,6 +84,7 @@ const ChartComponenet = ({ data, type }) => {
         />
         <CartesianGrid stroke="#404042" />
         <YAxis dataKey={type} domain={("auto", "auto")} />
+
         <Legend />
         <Tooltip />
       </LineChart>
